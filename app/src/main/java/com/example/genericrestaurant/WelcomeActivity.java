dcaf2404df0d37f73a1df57a22822aa9348ef20a
amannagarkar <amannagarkar@gmail.com>
2019-04-02 20:30:42 +0530

    Added a splash screen. Changed the app theme. Added a new icon.

---
+++ app/src/main/java/com/example/genericrestaurant/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.example.genericrestaurant;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.welcome_animation);
+        TextView textView = findViewById(R.id.textView12);
+        textView.startAnimation(animation);
+
+
+    }
+}
